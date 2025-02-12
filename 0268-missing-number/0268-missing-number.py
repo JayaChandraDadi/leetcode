1fe3a2dc977bdb1b9fc992cd6388feabe85eb3c1
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-02-12 00:51:07 -0500

    Time: 3 ms (65%), Space: 13.5 MB (30.25%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        xor1 = 0
+        xor2 = 0
+        for i in range(len(nums)):
+            xor2 = xor2^nums[i]
+            xor1 = xor1^(i+1)
+        return xor1^xor2
+        
