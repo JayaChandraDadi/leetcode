c44982b6550f29fd04580eb9beff466418b7a5ea
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-02-12 00:53:35 -0500

    Time: 11 ms (53.54%), Space: 14.5 MB (10.41%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        ct = 0
+        el = -1
+        for i in range(len(nums)):
+            if ct==0:
+                el = nums[i]
+                ct+=1
+            elif nums[i]==el:
+                ct+=1
+            else:
+                ct-=1
+        return el
+        
