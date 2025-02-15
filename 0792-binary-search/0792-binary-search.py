e49b7234ff56ca3eea45baf188d20cd86f6300bb
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-02-14 19:52:33 -0500

    Time: 0 ms (100%), Space: 13.4 MB (62.75%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,15 +1,15 @@
 class Solution(object):
+    def binsearch(self,nums,low,high,target):
+        if low>high:
+            return -1
+        mid = (low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif target>nums[mid]:
+            return self.binsearch(nums,mid+1,high,target)
+        else:
+            return self.binsearch(nums,low,mid-1,target)
     def search(self, nums, target):
-        low = 0
-        high = len(nums)-1
-        while(low<=high):
-            mid = (low+high)//2
-            if target>nums[mid]:
-                low = mid+1
-            elif target<nums[mid]:
-                high = mid-1
-            else:
-                return mid
-        return -1
+        return self.binsearch(nums,0,len(nums)-1,target)
     
         
