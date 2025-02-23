48fc35f62f9bdd898dddaf59438526b1abdf92ba
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-02-23 13:32:43 -0500

    Time: 9 ms (32.43%), Space: 24.1 MB (22.28%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,7 +5,7 @@ class Solution(object):
         temp = []
         for i in range(n-k,n):
             temp.append(arr[i])
-        for i in range(n,k,-1):
-            arr[i-1] = arr[i-k-1]
+        for i in range(n-1,k-1,-1):
+            arr[i]=arr[i-k]
         for i in range(k):
             arr[i] = temp[i]
