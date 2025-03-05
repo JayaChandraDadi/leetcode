2c65b93c6680fe3ffbfbb8e6a6aece162c2071d0
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-03-05 18:10:07 -0500

    Time: 5 ms (42.82%), Space: 12.6 MB (51.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,49 +1,41 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        n = len(nums1)+len(nums2)
-        ct = 0
-        index1 = n//2
-        index2 = n//2-1
-        i = 0
-        j = 0
-        el1 = -1
-        el2 = -1
-        while(i<len(nums1) and j<len(nums2)):
-            if nums1[i]<nums2[j]:
-                if index1==ct:
-                    el1 = nums1[i]
-                if index2==ct:
-                    el2 = nums1[i]
-                ct+=1
-                i+=1
+        if len(nums1)>len(nums2):
+            nums1,nums2 = nums2,nums1
+        n1 = len(nums1)
+        n2 = len(nums2)
+        low = 0
+        left = (n1+n2+1)//2
+        high = n1
+        n = n1+n2
+        while(low<=high):
+            mid1 = (low+high)//2
+            mid2 = left-mid1
+            l1 = float("-inf")
+            l2 = float("-inf")
+            r1 = float("inf")
+            r2 = float("inf")
+            if mid1<n1:
+                r1 = nums1[mid1]
+            if mid2<n2:
+                r2 = nums2[mid2]
+            if mid1-1>=0:
+                l1 = nums1[mid1-1]
+            if mid2-1>=0:
+                l2 = nums2[mid2-1]
+            if l1<=r2 and l2<=r1:
+                if n%2==0:
+                    return (max(l1,l2)+min(r1,r2))/2.0
+                else:
+                    return max(l1,l2)
+            elif l1>r2:
+                high = mid1-1
             else:
-                if index1==ct:
-                    el1 = nums2[j]
-                if index2==ct:
-                    el2 = nums2[j]
-                ct+=1
-                j+=1
-        while(i<len(nums1)):
-            if index1==ct:
-                    el1 = nums1[i]
-            if index2==ct:
-                    el2 = nums1[i]
-            ct+=1
-            i+=1
-        while(j<len(nums2)):
-            if index1==ct:
-                    el1 = nums2[j]
-            if index2==ct:
-                    el2 = nums2[j]
-            ct+=1
-            j+=1
-        if n%2==1:
-            return el1
-        else:
-            return (el1+el2)/2.0
-
-
-
+                low = mid1+1
+        return -1
 
             
+
+
+
         
