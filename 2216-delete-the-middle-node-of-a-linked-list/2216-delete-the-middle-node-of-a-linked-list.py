e1e7809ceb9ec645fbd1e2702721496965f10d76
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-03-12 16:26:39 -0400

    Time: 94 ms (27.43%), Space: 92 MB (26.54%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
@@ -5,14 +5,13 @@
 #         self.next = next
 class Solution(object):
     def deleteMiddle(self, head):
-        fast = head
-        slow = head
         if head==None or head.next==None:
             return 
+        fast = head.next.next
+        slow = head
         while(fast!=None and fast.next!=None):
-            temp = slow
             slow = slow.next
             fast = fast.next.next
-        temp.next = temp.next.next
+        slow.next = slow.next.next
         return head
         
