6d0db4be74a5c5ebadb96c8c0ba7d465c6c5027e
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-03-29 11:11:44 -0400

    Time: 0 ms (100%), Space: 12.4 MB (52.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,19 +1,18 @@
 class Solution(object):
     def nextPermutation(self, nums):
-        n = len(nums)
         pt = -1
-        for i in range(n-2,-1,-1):
+        for i in range(len(nums)-2,-1,-1):
             if nums[i]<nums[i+1]:
                 pt = i
                 break
-        if pt==-1:
-            nums.reverse()
-            return nums
-        for i in range(n-1,pt,-1):
-            if nums[i]>nums[pt]:
-                nums[pt],nums[i] = nums[i],nums[pt]
-                break
-        nums[pt+1:] = reversed(nums[pt+1:])
-        return nums
+        if pt == -1:
+           nums.reverse()
+        else:
+            for i in range(len(nums)-1,-1,-1):
+                if nums[i]>nums[pt]:
+                    nums[i],nums[pt] = nums[pt],nums[i]
+                    break
+            nums[pt+1:] = reversed(nums[pt+1:])
+        
 
         
