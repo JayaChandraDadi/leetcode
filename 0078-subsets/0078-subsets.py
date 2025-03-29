f0f08658d1e3b16d33f299155a74e3b6361663bc
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-03-29 17:30:12 -0400

    Time: 0 ms (100%), Space: 12.7 MB (19.09%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,7 +2,7 @@ class Solution(object):
     def subsets(self, nums):
         n = len(nums)
         res = []
-        for num in range(1<<n):
+        for num in range(2**n):
             subset=[]
             for i in range(n):
                 if num&(1<<i):
