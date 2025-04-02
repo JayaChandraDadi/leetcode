858321c1e5f72fcbe17eb3285b3eaa50f5472af3
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-04-02 12:29:43 -0400

    Time: 3483 ms (94.48%), Space: 12.4 MB (83.06%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,32 +1,31 @@
 class Solution(object):
-    def analyze(self,row,col,board,word,temp,i,n,vis,m):
+    def analyze(self,row,col,board,word,i,n,vis,m):
         if i==len(word):
             return True
         if row+1<m and board[row+1][col]==word[i] and vis[row+1][col]!=1:
             vis[row+1][col] = 1
-            if self.analyze(row+1,col,board,word,temp,i+1,n,vis,m):
+            if self.analyze(row+1,col,board,word,i+1,n,vis,m):
                 return True
             vis[row+1][col] = 0
         if col-1>=0 and board[row][col-1]==word[i] and vis[row][col-1]!=1:
             vis[row][col-1] = 1
-            if self.analyze(row,col-1,board,word,temp,i+1,n,vis,m):
+            if self.analyze(row,col-1,board,word,i+1,n,vis,m):
                 return True
             vis[row][col-1] = 0
         if col+1<n and board[row][col+1]==word[i] and vis[row][col+1]!=1:
             vis[row][col+1] = 1
-            if self.analyze(row,col+1,board,word,temp,i+1,n,vis,m):
+            if self.analyze(row,col+1,board,word,i+1,n,vis,m):
                 return True
             vis[row][col+1] = 0
         if row-1>=0 and board[row-1][col]==word[i] and vis[row-1][col]!=1:
             vis[row-1][col] = 1
-            if self.analyze(row-1,col,board,word,temp,i+1,n,vis,m):
+            if self.analyze(row-1,col,board,word,i+1,n,vis,m):
                 return True
             vis[row-1][col] = 0
         return False
         
     def exist(self, board, word):
         word = list(word)
-        temp = []
         m = len(board)
         n = len(board[0])
         vis = [[0]*n for _ in range(m)]
@@ -34,7 +33,7 @@ class Solution(object):
             for row in range(m):
                 if board[row][col]==word[0]:
                     vis[row][col] = 1
-                    if self.analyze(row,col,board,word,temp,1,n,vis,m):
+                    if self.analyze(row,col,board,word,1,n,vis,m):
                         return True
                     vis[row][col] = 0
         return False
