804439fccb8a93fb1e734254806ce4bb3803de4a
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-04-02 12:29:00 -0400

    Time: 3421 ms (94.48%), Space: 12.6 MB (18.29%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,26 @@
 class Solution(object):
     def analyze(self,row,col,board,word,temp,i,n,vis,m):
-        if word==temp:
+        if i==len(word):
             return True
         if row+1<m and board[row+1][col]==word[i] and vis[row+1][col]!=1:
-            temp.append(board[row+1][col])
             vis[row+1][col] = 1
             if self.analyze(row+1,col,board,word,temp,i+1,n,vis,m):
                 return True
-            temp.pop()
             vis[row+1][col] = 0
         if col-1>=0 and board[row][col-1]==word[i] and vis[row][col-1]!=1:
-            temp.append(board[row][col-1])
             vis[row][col-1] = 1
             if self.analyze(row,col-1,board,word,temp,i+1,n,vis,m):
                 return True
-            temp.pop()
             vis[row][col-1] = 0
         if col+1<n and board[row][col+1]==word[i] and vis[row][col+1]!=1:
-            temp.append(board[row][col+1])
             vis[row][col+1] = 1
             if self.analyze(row,col+1,board,word,temp,i+1,n,vis,m):
                 return True
-            temp.pop()
             vis[row][col+1] = 0
         if row-1>=0 and board[row-1][col]==word[i] and vis[row-1][col]!=1:
-            temp.append(board[row-1][col])
             vis[row-1][col] = 1
             if self.analyze(row-1,col,board,word,temp,i+1,n,vis,m):
                 return True
-            temp.pop()
             vis[row-1][col] = 0
         return False
         
@@ -42,10 +34,8 @@ class Solution(object):
             for row in range(m):
                 if board[row][col]==word[0]:
                     vis[row][col] = 1
-                    temp.append(board[row][col])
                     if self.analyze(row,col,board,word,temp,1,n,vis,m):
                         return True
                     vis[row][col] = 0
-                    temp.pop()
         return False
 
