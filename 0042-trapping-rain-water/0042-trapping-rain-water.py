e8bf6fe0996a0b05e804379938a42b2e372e71c1
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-04-23 15:38:22 -0400

    Time: 3 ms (99.04%), Space: 13.4 MB (80.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,21 @@
 class Solution(object):
     def trap(self, height):
         total = 0
-        prefixmax = [-1]*len(height)
-        prefixmax[0] = height[0]
-        for i in range(1,len(height)-1):
-            prefixmax[i] = max(prefixmax[i-1],height[i])
-        suffixmax = [-1]*len(height)
-        suffixmax[len(height)-1] = height[len(height)-1]
-        for i in range(len(height)-2,-1,-1):
-            suffixmax[i] = max(suffixmax[i+1],height[i])
-        for i in range(len(height)):
-            leftmax = prefixmax[i]
-            rightmax = suffixmax[i]
-            if height[i]<leftmax and height[i]<rightmax:
-                total+=min(leftmax,rightmax)-height[i]
+        leftmax = 0
+        rightmax = 0
+        l = 0
+        r = len(height)-1
+        while(l<r):
+            if height[l]<=height[r]:
+                if leftmax>height[l]:
+                    total+=leftmax-height[l]
+                else:
+                    leftmax = height[l]
+                l+=1
+            else:
+                if rightmax>height[r]:
+                    total+=rightmax-height[r]
+                else:
+                    rightmax = height[r]
+                r-=1
         return total
-
-
-        
