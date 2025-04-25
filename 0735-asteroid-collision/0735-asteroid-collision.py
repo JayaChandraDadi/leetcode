9466bc35868a39dfa760cea4432596b2700f74f0
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-04-25 01:24:40 -0400

    Time: 11 ms (48.62%), Space: 13.3 MB (63.4%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def asteroidCollision(self, nums):
+       st = []
+       for i in range(len(nums)):
+        if nums[i]>0:
+            st.append(nums[i])
+        else:
+            while st and st[-1]>0 and st[-1]<abs(nums[i]):
+                st.pop()
+            if st and st[-1]==abs(nums[i]):
+                st.pop()
+            elif not st or st[-1]<0:
+                st.append(nums[i])
+       return st
