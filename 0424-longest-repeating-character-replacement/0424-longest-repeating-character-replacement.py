c93e5326c97aa8498d3c2b5b7ee5050f42a5f002
Jaya Chandra Dadi <112895292+Jaichandra242003-aiml@users.noreply.github.com>
2025-05-09 16:57:07 -0400

    Time: 144 ms (54.24%), Space: 13 MB (73.6%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        mpp = {}
+        l = 0
+        r = 0
+        maxf = 0
+        maxlen = 0
+        while(r<len(s)):
+            if s[r] not in mpp:
+                mpp[s[r]] = 1
+            else:
+                mpp[s[r]]+=1
+            maxf = max(maxf,mpp[s[r]])
+            if r-l+1-maxf<=k:
+                maxlen = max(maxf,r-l+1)
+            if r-l+1-maxf>k:
+                mpp[s[l]]-=1
+                l+=1
+            r+=1
+        return maxlen
