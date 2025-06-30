6132acdeb7f0f3190c64660371d513e2e12ca6be
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-06-30 14:54:32 -0500

    Time: 227 ms (41.48%), Space: 15.9 MB (59.65%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,33 @@
+from collections import deque
+class Solution(object):
+    def updateMatrix(self, mat):
+        queue = deque()
+        m = len(mat)
+        n = len(mat[0] if mat else 0)
+        dist = mat
+        vis = []
+        for i in range(m):
+            temp = []
+            for j in range(n):
+                temp.append(0)
+            vis.append(temp)
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]==0:
+                    queue.append([i,j,0])
+                    vis[i][j] = 1
+        drow = [-1,0,+1,0]
+        dcol = [0,+1,0,-1]
+        while(len(queue)!=0):
+            r,c,step = queue.popleft()
+            dist[r][c] = step
+            for i in range(4):
+                nrow = r+drow[i]
+                ncol = c+dcol[i]
+                if nrow>=0 and nrow<m and ncol>=0 and ncol<n and mat[nrow][ncol]==1 and vis[nrow][ncol]==0:
+                    queue.append([nrow,ncol,step+1])
+                    vis[nrow][ncol] = 1
+        return dist
+
+        
+        
