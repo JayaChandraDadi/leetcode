9f251cc30d5334105472b5d25da0ff6cbcd8f37f
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-07-05 01:50:12 -0500

    Time: 6 ms (65.72%), Space: 14.8 MB (42.02%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def dfs(self, adj, node, vis, pathvis, stack):
+        vis[node] = 1
+        pathvis[node] = 1
+        for neighbour in adj[node]:
+            if vis[neighbour] == 0:
+                if self.dfs(adj, neighbour, vis, pathvis, stack):
+                    return True
+            elif pathvis[neighbour] == 1:
+                return True  # cycle detected
+        pathvis[node] = 0
+        stack.append(node)
+        return False
+
+    def findOrder(self, numCourses, prerequisites):
+        adj = [[] for _ in range(numCourses)]
+        for u, v in prerequisites:
+            adj[v].append(u)
+
+        vis = [0] * numCourses
+        pathvis = [0] * numCourses
+        stack = []
+
+        for i in range(numCourses):
+            if vis[i] == 0:
+                if self.dfs(adj, i, vis, pathvis, stack):
+                    return []  # cycle detected
+
+        return stack[::-1]  # topological order
