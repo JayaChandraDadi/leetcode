4fb7405ae841880eefe1d0e6ce3d2c9db1672d59
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-07-07 00:20:47 -0500

    Time: 1410 ms (5.03%), Space: 13.2 MB (89.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,30 @@
+from collections import deque
 class Solution(object):
-    def dfs(self,node,vis,pathvis,adj):
-        vis[node] = 1
-        pathvis[node] = 1
-        for neighbour in adj[node]:
-            if not vis[neighbour]:
-                if self.dfs(neighbour,vis,pathvis,adj):
-                    return True
-            if pathvis[neighbour]:
-                return True
-        pathvis[node] = 0
-        return False
     def canFinish(self, numCourses, prerequisites):
-        vis = [0]*numCourses
+        indegree = [0]*numCourses
         pathvis = [0]*numCourses
+        topo = []
+        queue = deque()
         for i in range(numCourses):
             adj = [[] for _ in range(numCourses)]
-            for u,v in prerequisites:
-                adj[v].append(u)
-            if not vis[i]:
-                if self.dfs(i,vis,pathvis,adj):
-                    return False
-        return True
-        
+        for u,v in prerequisites:
+            adj[v].append(u)
+        for i in range(numCourses):
+            for neighbour in adj[i]:
+                indegree[neighbour]+=1
+        for i in range(numCourses):
+            if indegree[i]==0:
+                queue.append(i)
+                topo.append(i)
+        while(len(queue)!=0):
+            node = queue.popleft()
+            for neighbour in adj[node]:
+                indegree[neighbour]-=1
+                if indegree[neighbour]==0:
+                    queue.append(neighbour)
+                    topo.append(neighbour)
+        if len(topo)==numCourses:
+            return True
+        return False
+
+            
