f6a5c5cb78be5384742ce4728dfcc26b5e0bca75
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-07-21 15:37:30 -0500

    Time: 15 ms (75.95%), Space: 12.4 MB (79.46%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -3,10 +3,13 @@ class Solution(object):
     def fib(self, n):
         if n==0:
             return 0
-        dp = [-1]*(n+1)
-        dp[0] = 0
-        dp[1] = 1
+        if n==1:
+            return 1
+        prev2 = 0
+        prev = 1
         for i in range(2,n+1):
-            dp[i] = dp[i-1]+dp[i-2]
-        return dp[n]
+            dp = prev+prev2
+            prev2 = prev
+            prev = dp
+        return dp
         
