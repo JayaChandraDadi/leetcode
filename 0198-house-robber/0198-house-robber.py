a74c3b5d129947a9d769bc277708542fbacce865
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-07-23 15:56:51 -0500

    Time: 0 ms (100%), Space: 12.5 MB (54.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,17 @@
 class Solution(object):
     def rob(self, nums):
         n = len(nums)
-        dp = [-1]*n
-        dp[0] = nums[0]
+        prev1 = nums[0]
+        prev2 = 0
+        curri  = nums[0]
         for i in range(1,n):
             if i-2<0:
                 take = nums[i]
             else:
-                take = nums[i] + dp[i-2]
-            nottake = dp[i-1]
-            dp[i] = max(take,nottake)
-        return dp[n-1]
+                take = nums[i] + prev2
+            nottake = prev1
+            curri = max(take,nottake)
+            prev2 = prev1
+            prev1 = curri
+        return curri
         
