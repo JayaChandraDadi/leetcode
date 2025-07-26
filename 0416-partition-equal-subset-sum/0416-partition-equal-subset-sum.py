006f436b5d767d6767b9bd0dd5fa0677fdd5037e
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-07-26 18:21:58 -0500

    Time: 2463 ms (25.11%), Space: 28.6 MB (32.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -17,6 +17,17 @@ class Solution(object):
         if sum1%2==1:
             return False
         n = len(nums)
-        sum1=sum1//2
-        dp = [[None]*(sum1+1) for _ in range(n+1)]
-        return self.subset(nums,sum1,n-1,dp)
+        target=sum1//2
+        dp = [[False]*(target+1) for _ in range(n+1)]
+        for i in range(n):
+            dp[i][0] = True
+        if target>=nums[0]:
+            dp[0][nums[0]] = True
+        for i in range(1,n):
+            for j in range(1,target+1):
+                nottake = dp[i-1][j]
+                take = False
+                if j>=nums[i]:
+                    take = dp[i-1][j-nums[i]]
+                dp[i][j] = (nottake or take)
+        return dp[n-1][target]
