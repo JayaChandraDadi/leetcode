aa8ee663f7c3887dc979ae9eadfc01f4771dd68e
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-07-30 19:02:52 -0500

    Time: 1099 ms (19.14%), Space: 22 MB (88.47%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def maxlength(self,ind1,ind2,s1,s2,dp):
-        if ind1<0 or ind2<0:
+        if ind1==0 or ind2==0:
             return 0
         if dp[ind1][ind2]!=-1:
             return dp[ind1][ind2]
-        if s1[ind1]==s2[ind2]:
+        if s1[ind1-1]==s2[ind2-1]:
             dp[ind1][ind2] = 1+self.maxlength(ind1-1,ind2-1,s1,s2,dp)
             return dp[ind1][ind2]
         dp[ind1][ind2] = max(self.maxlength(ind1-1,ind2,s1,s2,dp),self.maxlength(ind1,ind2-1,s1,s2,dp))
@@ -12,6 +12,6 @@ class Solution(object):
     def longestCommonSubsequence(self, text1, text2):
         n1 = len(text1)
         n2 = len(text2)
-        dp = [[-1]*(n2) for _ in range(n1)]
-        return self.maxlength(n1-1,n2-1,text1,text2,dp)
+        dp = [[-1]*(n2+1) for _ in range(n1+1)]
+        return self.maxlength(n1,n2,text1,text2,dp)
         
