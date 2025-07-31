3d64649351a077942968171d2c7d1d4e29585914
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-07-31 15:44:17 -0500

    Time: 1404 ms (50.19%), Space: 12.6 MB (93.05%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -14,6 +14,14 @@ class Solution(object):
         n2 = n1
         s1 = s
         s2 = s[::-1]
-        dp = [[-1]*(n2+1) for _ in range(n1+1)]
-        return self.maxlength(n1,n2,s1,s2,dp)
+        prev = [0]*(n2+1) 
+        for i in range(1,n1+1):
+            temp = [0]*(n2+1)
+            for j in range(1,n2+1):
+                if s1[i-1]==s2[j-1]:
+                    temp[j] = 1+prev[j-1]
+                else:
+                    temp[j] = max(prev[j],temp[j-1])
+            prev = temp
+        return prev[n2]
         
