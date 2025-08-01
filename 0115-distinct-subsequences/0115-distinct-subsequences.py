830dd30b703cc57d5f4a812dfc3de08270169eda
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-08-01 16:07:10 -0500

    Time: 530 ms (48.74%), Space: 69.5 MB (27.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def count(self,s,t,i,j,dp):
-        if j<0:
+        if j==0:
             return 1
-        if i<0:
+        if i==0:
             return 0
         if dp[i][j]!=-1:
             return dp[i][j]
-        if s[i]==t[j]:
+        if s[i-1]==t[j-1]:
             dp[i][j]=self.count(s,t,i-1,j-1,dp)+self.count(s,t,i-1,j,dp)
         else:
             dp[i][j] = self.count(s,t,i-1,j,dp)
@@ -14,6 +14,15 @@ class Solution(object):
     def numDistinct(self, s, t):
         n1 = len(s)
         n2 = len(t)
-        dp = [[-1]*(n2) for _ in range(n1)]
-        return self.count(s,t,n1-1,n2-1,dp)
+        dp = [[0]*(n2+1) for _ in range(n1+1)]
+        for i in range(n1+1):
+            dp[i][0] = 1
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j] = self.count(s,t,i-1,j,dp)
+        return dp[n1][n2]
+        return self.count(s,t,n1,n2,dp)
         
