aeec751c8c0e4bc14963b0e7991efd7f7cd04ce8
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-08-05 18:58:22 -0500

    Time: 396 ms (87.25%), Space: 21.7 MB (12.84%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,20 +1,20 @@
 class Solution(object):
     def match(self,i,j,s1,s2,dp):
-        if i<0 and j<0:
+        if i==0 and j==0:
             return True
-        if i<0 and j>=0:
+        if i==0 and j>0:
             return False
-        if i>=0 and j<0:
-            for k in range(i+1):
-                if s1[k]!='*':
+        if i>0 and j==0:
+            for k in range(1,i+1):
+                if s1[k-1]!='*':
                     return False
             return True
         if dp[i][j]!=None:
             return dp[i][j]
-        if s1[i]==s2[j] or s1[i]=='?':
+        if s1[i-1]==s2[j-1] or s1[i-1]=='?':
             dp[i][j] = self.match(i-1,j-1,s1,s2,dp)
             return dp[i][j]
-        if s1[i]=='*':
+        if s1[i-1]=='*':
             dp[i][j] = self.match(i-1,j,s1,s2,dp) or self.match(i,j-1,s1,s2,dp)
             return dp[i][j]
         dp[i][j] = False
@@ -22,7 +22,7 @@ class Solution(object):
     def isMatch(self, s, p):
         n = len(s)
         m = len(p)
-        dp = [[None]*(n) for _ in range(m)]
-        return self.match(m-1,n-1,p,s,dp)
+        dp = [[None]*(n+1) for _ in range(m+1)]
+        return self.match(m,n,p,s,dp)
 
         
