9025eda93686e8aa0ffc3f5e1ca0e0ef5e013312
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-08-11 00:02:25 -0500

    Time: 7036 ms (25.98%), Space: 15 MB (39.71%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maximum(self,i,j,nums,dp):
+        if i>j:
+            return 0
+        if dp[i][j]!=-1:
+            return dp[i][j]
+        maxi = float('-inf')
+        for ind in range(i,j+1):
+            coins = (nums[i-1]*nums[ind]*nums[j+1]) + self.maximum(i,ind-1,nums,dp) + self.maximum(ind+1,j,nums,dp)
+            maxi = max(maxi,coins)
+        dp[i][j] = maxi
+        return dp[i][j]
+    def maxCoins(self, nums):
+        nums.append(1)
+        nums = [1] + nums
+        n = len(nums)
+        dp = [[-1]*(n) for _ in range(n)]
+        return self.maximum(1,n-2,nums,dp)
