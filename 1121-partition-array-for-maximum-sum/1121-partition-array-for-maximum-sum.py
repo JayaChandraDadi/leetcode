2123664a61048632a2b95f79d26de53eefffef4e
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-08-16 21:44:16 -0500

    Time: 440 ms (21.78%), Space: 16.3 MB (23.76%) - LeetHub

---
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxsum(self,i,n,arr,k,dp):
+        if i==n:
+            return 0
+        if dp[i]!=-1:
+            return dp[i]
+        length = 0
+        sum1 = 0
+        maxi = float('-inf')
+        maximumsum = float('-inf')
+        for j in range(i,min(n,i+k)):
+            length+=1
+            maxi = max(maxi,arr[j])
+            sum = 0
+            sum1 = length*maxi + self.maxsum(j+1,n,arr,k,dp)
+            maximumsum = max(maximumsum,sum1)
+        dp[i] = maximumsum
+        return dp[i]
+    def maxSumAfterPartitioning(self, arr, k):
+        n = len(arr)
+        dp = [-1]*(n)
+        return self.maxsum(0,n,arr,k,dp)
+        
+        
