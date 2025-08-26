be8652487b4b18520a9d70d35c6041df719ccc47
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-08-26 16:51:59 -0500

    Time: 99 ms (41.41%), Space: 19.6 MB (10.6%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def maxProfit(self, nums):
+        n = len(nums)
         buy = nums[0]
         maxprofit = 0
-        for i in range(1,len(nums)):
-            maxprofit = max(maxprofit,nums[i]-buy)
-            buy = min(buy,nums[i])
+        for i in range(len(nums)):
+            profit = nums[i]-buy
+            maxprofit = max(maxprofit,profit)
+            if nums[i]<buy:
+                buy = nums[i]
         return maxprofit
-        
