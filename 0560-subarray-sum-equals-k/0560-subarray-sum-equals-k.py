b56e572805ab404682476850cc7a500b189495fa
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-08-27 08:06:19 -0500

    Time: 36 ms (63.92%), Space: 15.3 MB (24.62%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,18 @@
 class Solution(object):
     def subarraySum(self, nums, k):
-        ct = 0
         sum1 = 0
+        ct = 0
+        n = len(nums)
         presummap = {}
-        presummap[0] = 1
-        #rem = -3
-        for i in range(len(nums)):
+        for i in range(n):
             sum1+=nums[i]
+            if sum1==k:
+                ct+=1
             rem = sum1-k
             if rem in presummap:
                 ct+=presummap[rem]
             if sum1 not in presummap:
-                presummap[sum1] =1
+                presummap[sum1]=1
             else:
-                presummap[sum1]+= 1
+                presummap[sum1]+=1
         return ct
-        
