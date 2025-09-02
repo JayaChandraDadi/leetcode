358176a6458dcb977226a7192c1795ed22eb98ee
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-02 13:41:44 -0500

    Time: 177 ms (34.85%), Space: 12.7 MB (92.78%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,20 +1,21 @@
 class Solution(object):
     def characterReplacement(self, s, k):
-        mpp = {}
-        l = 0
-        r = 0
-        maxf = 0
-        maxlen = 0
-        while(r<len(s)):
-            if s[r] not in mpp:
-                mpp[s[r]] = 1
-            else:
-                mpp[s[r]]+=1
-            maxf = max(maxf,mpp[s[r]])
-            if r-l+1-maxf<=k:
-                maxlen = max(maxlen,r-l+1)
-            if r-l+1-maxf>k:
-                mpp[s[l]]-=1
+       n = len(s)
+       maxlen = 0
+       maxfreq = 0
+       l = 0
+       r = 0
+       hashmap = [0]*26
+       while(r<n):
+        hashmap[ord(s[r])-ord('A')]+=1
+        maxfreq = max(maxfreq,hashmap[ord(s[r])-ord('A')])
+        changes = r-l+1-maxfreq
+        if changes<=k:
+            maxlen = max(maxlen,r-l+1)
+        else:
+            while(changes>k):
+                hashmap[ord(s[l])-ord('A')]-=1
                 l+=1
-            r+=1
-        return maxlen
+                changes = maxfreq-(r-l+1)
+        r+=1
+       return maxlen
