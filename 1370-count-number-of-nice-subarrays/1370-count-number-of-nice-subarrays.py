ec22aba1a6fe18e67b3b8039abf3e91ce3f94a0d
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-02 21:10:59 -0500

    Time: 179 ms (14.65%), Space: 16.1 MB (90.15%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,34 +1,35 @@
 class Solution(object):
     def numberOfSubarrays(self, nums, goal):
+        n = len(nums)
+        oddcount = 0
         temp = []
-        for i in range(len(nums)):
-            temp.append(nums[i]%2)
-        if goal<0:
-            return -1
-        n = len(temp)
-        sum = 0
+        for i in range(n):
+            if nums[i]%2==1:
+                temp.append(1)
+            else:
+                temp.append(0)
         ct1 = 0
         l = 0
         r = 0
+        sum1 = 0
         while(r<n):
-            sum+=temp[r]
-            while(sum>goal):
-                sum-=temp[l]
+            sum1+=temp[r]
+            while(sum1>goal):
+                sum1-=temp[l]
                 l+=1
-            ct1 = ct1+(r-l+1)
+            ct1+=(r-l+1)
             r+=1
-        sum = 0
-        goal-=1
-        if goal<0:
-            return ct1
         ct2 = 0
         l = 0
         r = 0
+        sum2 = 0
+        if goal-1<0:
+            return ct1
         while(r<n):
-            sum+=temp[r]
-            while(sum>goal):
-                sum-=temp[l]
+            sum2+=temp[r]
+            while(sum2>goal-1):
+                sum2-=temp[l]
                 l+=1
-            ct2 = ct2+(r-l+1)
+            ct2+=(r-l+1)
             r+=1
         return ct1-ct2
