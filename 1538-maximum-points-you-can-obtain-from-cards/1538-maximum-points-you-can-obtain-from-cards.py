cf8ddd116ec821a818d0dd4c32c703352265af44
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-03 10:16:59 -0500

    Time: 31 ms (35.3%), Space: 19.3 MB (76.36%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -3,14 +3,15 @@ class Solution(object):
         lsum = 0
         rsum = 0
         maxsum = 0
-        r = len(nums)-1
+        n = len(nums)
+        r = n-1
         for i in range(k):
             lsum+=nums[i]
-        maxsum = lsum
-        for i in range(k-1,-1,-1):
-            lsum-=nums[i]
+        maxsum = max(maxsum,lsum)
+        for j in range(k-1,-1,-1):
+            lsum-=nums[j]
             rsum+=nums[r]
             r-=1
-            maxsum = max(maxsum,lsum+rsum)
+            maxsum = max(maxsum,rsum+lsum)
         return maxsum
         
