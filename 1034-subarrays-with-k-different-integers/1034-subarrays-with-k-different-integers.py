ab374cddd1619ece068738706e6eb78562b5407b
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-04 12:54:17 -0500

    Time: 203 ms (13.91%), Space: 13.9 MB (52.23%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.py
@@ -3,35 +3,37 @@ class Solution(object):
         l = 0
         r = 0
         ct1 = 0
-        mpp = {}
+        hashmap = {}
         while(r<len(nums)):
-            if nums[r] not in mpp:
-                mpp[nums[r]] = 1
-            else:
-                mpp[nums[r]]+=1
-            while(len(mpp)>k):
-                mpp[nums[l]]-=1
-                if mpp[nums[l]]==0:
-                    del mpp[nums[l]]
-                l+=1
+            if nums[r] not in hashmap:
+                hashmap[nums[r]] = 0
+            hashmap[nums[r]]+=1
+            if len(hashmap)>k:
+                while(len(hashmap)>k):
+                    hashmap[nums[l]]-=1
+                    if hashmap[nums[l]]==0:
+                        hashmap.pop(nums[l])
+                    l+=1
             ct1+=(r-l+1)
             r+=1
         l = 0
         r = 0
         ct2 = 0
-        mpp = {}
+        hashmap = {}
         while(r<len(nums)):
-            if nums[r] not in mpp:
-                mpp[nums[r]] = 1
-            else:
-                mpp[nums[r]]+=1
-            while(len(mpp)>k-1):
-                mpp[nums[l]]-=1
-                if mpp[nums[l]]==0:
-                    del mpp[nums[l]]
-                l+=1
+            if nums[r] not in hashmap:
+                hashmap[nums[r]] = 0
+            hashmap[nums[r]]+=1
+            if len(hashmap)>k-1:
+                while(len(hashmap)>k-1):
+                    hashmap[nums[l]]-=1
+                    if hashmap[nums[l]]==0:
+                        hashmap.pop(nums[l])
+                    l+=1
             ct2+=(r-l+1)
             r+=1
         return ct1-ct2
         
+                
 
+        
