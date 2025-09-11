79b62f963435b7119ff928e50871d07245f4f568
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-10 21:48:15 -0500

    Time: 170 ms (61.43%), Space: 16.1 MB (56.57%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.py
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.py
@@ -1,31 +1,30 @@
 class Solution(object):
-    def sumSubarrayMins(self, arr):
-        n = len(arr)
-        nse = [-1]*n
-        pse = [-1]*n
+    def sumSubarrayMins(self, nums):
+        n = len(nums)
+        nse = [-1]*(n)
         st = []
-        total = 0
+        MOD = 10**9+7
         for i in range(n-1,-1,-1):
-            while st and arr[st[-1]]>arr[i]:
+            while st and nums[i]<nums[st[-1]]:
                 st.pop()
-            if st:
-                nse[i] = st[-1]
-            else:
+            if not st:
                 nse[i] = n
+            else:
+                nse[i] = st[-1]
             st.append(i)
+        pse = [-1]*(n)
         st = []
         for i in range(n):
-            while st and arr[st[-1]]>=arr[i]:
+            while st and nums[i]<=nums[st[-1]]:
                 st.pop()
-            if st:
-                pse[i] = st[-1]
-            else:
+            if not st:
                 pse[i] = -1
+            else:
+                pse[i] = st[-1]
             st.append(i)
-        for i in range(n):
+        total = 0
+        for i in range(len(nums)):
             left = i-pse[i]
-            right = nse[i] - i
-            total=(total+left*right*arr[i])%((10**9)+7)
+            right = nse[i]-i
+            total=(total+left*right*nums[i])%MOD
         return total
-
-            
