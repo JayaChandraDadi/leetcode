88f9003d6ce10e5c2b487744392b96ff4965b21b
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-13 00:30:28 -0500

    Time: 35 ms (70.54%), Space: 12.6 MB (77.52%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
@@ -1,56 +1,54 @@
 class Solution(object):
-    def subArrayRanges(self, arr):
-        n = len(arr)
-        nse = [-1]*n
+    def subArrayRanges(self, nums):
+        n = len(nums)
+        st = []
         pse = [-1]*n
-        st = []
-        nge = [-1]*n
         pge = [-1]*n
-        totalmin = 0
-        totalmax = 0
+        nge = [-1]*n 
+        nse = [-1]*n
         for i in range(n-1,-1,-1):
-            while st and arr[st[-1]]>arr[i]:
+            while st and nums[st[-1]]<=nums[i]:
                 st.pop()
-            if st:
-                nse[i] = st[-1]
-            else:
-                nse[i] = n
-            st.append(i)
-        st = []
-        for i in range(n):
-            while st and arr[st[-1]]>=arr[i]:
-                st.pop()
-            if st:
-                pse[i] = st[-1]
-            else:
-                pse[i] = -1
-            st.append(i)
-        st = []
-        for i in range(n):
-            while st and arr[st[-1]]<=arr[i]:
-                st.pop()
-            if st:
-                pge[i] = st[-1]
-            else:
-                pge[i] = -1
-            st.append(i)
-        st = []
-        for i in range(n-1,-1,-1):
-            while st and arr[st[-1]]<arr[i]:
-                st.pop()
-            if st:
-                nge[i] = st[-1]
-            else:
+            if not st:
                 nge[i] = n
+            else:
+                nge[i] = st[-1]
             st.append(i)
+        st = []
+        for i in range(n-1,-1,-1):
+            while st and nums[st[-1]]>nums[i]:
+                st.pop()
+            if not st:
+                nse[i] = n
+            else:
+                nse[i] = st[-1]
+            st.append(i)
+        st = []
+        for i in range(n):
+            while st and nums[st[-1]]<nums[i]:
+                st.pop()
+            if not st:
+                pge[i] = -1
+            else:
+                pge[i] = st[-1]
+            st.append(i)
+        st = []
+        for i in range(n):
+            while st and nums[st[-1]]>=nums[i]:
+                st.pop()
+            if not st:
+                pse[i] = -1
+            else:
+                pse[i] = st[-1]
+            st.append(i)
+        leftrange =0
+        rightrange = 0
         for i in range(n):
             leftmin = i-pse[i]
-            rightmin = nse[i] - i
-            leftmax = i - pge[i]
-            rightmax = nge[i] - i
-            totalmin+=(leftmin*rightmin*arr[i])
-            totalmax+=(leftmax*rightmax*arr[i])
-        return totalmax-totalmin
-
-        
+            rightmin = nse[i]-i
+            leftmax = i-nge[i]
+            rightmax = pge[i]-i
+            leftrange+=(leftmin*rightmin*nums[i])
+            rightrange+=(leftmax*rightmax*nums[i])
+        return rightrange-leftrange
         
