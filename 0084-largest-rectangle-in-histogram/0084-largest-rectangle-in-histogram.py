b889bba09502dc26975f3e3b6540817db14a691a
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-13 18:45:25 -0500

    Time: 211 ms (56.6%), Space: 20.9 MB (68.87%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,27 +1,18 @@
 class Solution(object):
     def largestRectangleArea(self, nums):
         n = len(nums)
-        nse = [-1]*n
         st = []
-        for i in range(n-1,-1,-1):
-            while st and nums[st[-1]]>=nums[i]:
-                st.pop()
-            if not st:
-                nse[i] = n
-            else:
-                nse[i] = st[-1]
-            st.append(i)
-        pse = [-1]*n
-        st = []
-        for i in range(n):
-            while st and nums[st[-1]]>=nums[i]:
-                st.pop()
-            if not st:
-                pse[i] = -1
-            else:
-                pse[i] = st[-1]
-            st.append(i)
         maxarea = float('-inf')
         for i in range(n):
-            maxarea = max(maxarea,(nse[i]-pse[i]-1)*nums[i])
+            while st and nums[st[-1]]>=nums[i]:
+                el = st.pop()
+                nse = i
+                pse = st[-1] if st else -1
+                maxarea = max(maxarea,(nse-pse-1)*nums[el])
+            st.append(i)
+        while st:
+            el = st.pop()
+            nse = n
+            pse = st[-1] if st else -1
+            maxarea = max(maxarea,(nse-pse-1)*nums[el])
         return maxarea
