d8b52b942c670cf356847cdbb08736a3eb747645
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-13 19:15:57 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,37 +1,36 @@
 class Solution(object):
-    def maxrectangle(self,arr):
-        n = len(arr)
+    def findmax(self, nums):
+        n = len(nums)
         st = []
-        maxarea = 0
+        maxarea = float('-inf')
         for i in range(n):
-            while st and arr[st[-1]]>arr[i]:
+            while st and nums[st[-1]]>=nums[i]:
                 el = st.pop()
                 nse = i
                 pse = st[-1] if st else -1
-                maxarea = max(maxarea,(arr[el])*(nse-pse-1))
+                maxarea = max(maxarea,(nse-pse-1)*nums[el])
             st.append(i)
         while st:
             el = st.pop()
             nse = n
             pse = st[-1] if st else -1
-            maxarea = max(maxarea,(arr[el])*(nse-pse-1))
+            maxarea = max(maxarea,(nse-pse-1)*nums[el])
         return maxarea
     def maximalRectangle(self, matrix):
         m = len(matrix)
-        n = len(matrix[0]) if m else 0
-        maxi = 0
-        presum = [[0 for _ in range(n)] for _ in range(m)]
-        for j in range(n):
-            sum1 = 0
-            for i in range(m):
-                if int(matrix[i][j])==0:
-                    sum1 = 0
-                else:
-                    sum1+=int(matrix[i][j])
-                presum[i][j] = sum1
+        n = len(matrix[0]) if matrix else 0
+        premat = [[0]*n for _ in range(m)]
         for i in range(m):
-            maxi = max(maxi,self.maxrectangle(presum[i]))
-        return maxi
+            presum = 0
+            for j in range(n):
+                if matrix[i][j]==0:
+                    premat[i][j] = 0
+                else:
+                    premat[i][j] = int(matrix[i][j])+(int(premat[i-1][j]) if i>0 else 0)
+        maxarea = float('-inf')
+        for i in range(m):
+            maxarea = max(maxarea,self.findmax(premat[i]))
+        return maxarea
 
 
         
