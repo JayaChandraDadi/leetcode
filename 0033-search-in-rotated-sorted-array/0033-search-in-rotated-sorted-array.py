2fcd4aacaaa9334face3ce8f1f85c6d35225c1cc
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-16 18:39:23 -0500

    Time: 4 ms (4.53%), Space: 12.5 MB (96.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -6,16 +6,14 @@ class Solution(object):
             mid = (low+high)//2
             if nums[mid]==target:
                 return mid
-            elif nums[mid]<=nums[high]:
-                if nums[mid]<=target<=nums[high]:
-                    low = mid+1
-                else:
-                    high = mid-1
-            else:
+            if nums[low]<=nums[mid]:
                 if nums[low]<=target<=nums[mid]:
                     high = mid-1
                 else:
                     low = mid+1
+            else:
+                if nums[mid]<=target<=nums[high]:
+                    low = mid+1
+                else:
+                    high = mid-1
         return -1
-
-        
