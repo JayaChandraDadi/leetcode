1808a72d00a44c4b86c8b014a0548ce1a1b60466
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-16 19:07:40 -0500

    Time: 0 ms (100%), Space: 16.6 MB (36.63%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,21 +1,26 @@
 class Solution(object):
     def singleNonDuplicate(self, nums):
-        low = 1
-        high = len(nums)-2
+        low = 0
+        n = len(nums)
+        high = len(nums)-1
         if len(nums)==1:
             return nums[0]
         if nums[0]!=nums[1]:
             return nums[0]
-        if nums[len(nums)-1]!=nums[len(nums)-2]:
-            return nums[len(nums)-1]
+        if nums[n-1]!=nums[n-2]:
+            return nums[n-1]
         while(low<=high):
             mid = (low+high)//2
-            if nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]:
-                return nums[mid]
-            elif (mid%2==1 and nums[mid-1]==nums[mid]) or (mid%2==0 and nums[mid]==nums[mid+1]):
-                low = mid+1
-            else:
-                high = mid-1
-        return -1
-
-        
+            if mid<n and mid>0:
+                if nums[mid-1]!=nums[mid]!=nums[mid+1]:
+                    return nums[mid]
+                elif mid%2==0:
+                    if nums[mid]==nums[mid-1]:
+                        high = mid-1
+                    else:
+                        low = mid+1
+                else:
+                    if nums[mid]==nums[mid+1]:
+                        high = mid-1
+                    else:
+                        low = mid+1
