a24625108cd1ff2773661b46aeb91eee3f3a9d05
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-16 20:11:00 -0500

    Time: 614 ms (21.42%), Space: 20.7 MB (67.56%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,15 +1,16 @@
 class Solution(object):
-    def bq(self,bloomDay,k,day):
+    def boquets(self,bloomDay,k,day):
         ct = 0
-        bqct = 0
+        bq = 0
         for i in range(len(bloomDay)):
-            if bloomDay[i]<=day:
+            if day>=bloomDay[i]:
                 ct+=1
+                if ct==k:
+                    bq+=1
+                    ct = 0
             else:
-                bqct+=(ct//k)
                 ct = 0
-        bqct+=(ct//k)
-        return bqct
+        return bq
     def minDays(self, bloomDay, m, k):
         low = min(bloomDay)
         high = max(bloomDay)
@@ -17,9 +18,9 @@ class Solution(object):
             return -1
         while(low<=high):
             mid = (low+high)//2
-            x = self.bq(bloomDay,k,mid)
-            if x>=m:
-                high = mid-1
-            else:
+            bq = self.boquets(bloomDay,k,mid)
+            if bq<m:
                 low = mid+1
+            else:
+                high = mid-1
         return low
