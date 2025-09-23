6598a880838e4a0b479635ea33eac9d7fbc8ec29
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-09-23 00:06:45 -0500

    Time: 1 ms (32.7%), Space: 32.4 MB (88.59%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
@@ -1,26 +1,25 @@
 class Solution(object):
-    def func(self,mat,m,n,mid):
-        maxvalue = -1
-        index = -1
+    def findmax(self,mat,m,n,mid):
+        max1 = float('-inf')
         for i in range(m):
-            if mat[i][mid]>maxvalue:
-                maxvalue = mat[i][mid]
-                index = i
-        return index
+            if mat[i][mid]>max1:
+                max1 = mat[i][mid]
+                maxrow = i
+        return maxrow
     def findPeakGrid(self, mat):
-        m = len(mat)
-        n = len(mat[0]) if m>0 else 0
-        low = 0
-        high = n-1
-        while(low<=high):
-            mid = (low+high)//2
-            row = self.func(mat,m,n,mid)
-            left = mat[row][mid-1] if mid-1>=0 else -1
-            right = mat[row][mid+1] if mid+1<n else -1
-            if left<mat[row][mid] and mat[row][mid] > right:
-                return [row,mid]
-            elif left>=mat[row][mid]:
-                high = mid-1
-            else:
-                low = mid+1
-        
+       m = len(mat)
+       n = len(mat[0]) if m else 0
+       low = 0
+       high = n-1
+       while(low<=high):
+        mid = (low+high)//2
+        maxrow = self.findmax(mat,m,n,mid)
+        left = mat[maxrow][mid-1] if mid-1>=0 else -1
+        right = mat[maxrow][mid+1] if mid+1<n else 0
+        if left<mat[maxrow][mid]>right:
+            return [maxrow,mid]
+        elif left>=mat[maxrow][mid]:
+            high = mid-1
+        else:
+            low = mid+1
+       return -1
