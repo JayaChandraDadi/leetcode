7ccc12e166a1920c9ff6d5f6e7ddb9ce51f764b8
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-10-05 00:14:58 -0500

    Time: 44 ms (12.17%), Space: 18.6 MB (6.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        res = defaultdict(list)
-        for word in strs:
+        n = len(strs)
+        hashmap = {}
+        for i in range(n):
             temp = [0]*26
-            for c in word:
-                temp[ord(c)-ord('a')]+=1
-            res[tuple(temp)].append(word)
-        return list(res.values())
+            for c in strs[i]:
+                temp[ord(c)-ord('a')]+= 1
+            key = tuple(temp)
+            if key not in hashmap:
+                hashmap[key] = []
+            hashmap[key].append(strs[i])
+        return sorted(list(hashmap.values()))
+
+        
