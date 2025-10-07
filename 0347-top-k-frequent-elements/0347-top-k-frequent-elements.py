725688eed0ca744cd3a1b2b5084845aeab623421
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-10-06 22:12:39 -0500

    Time: 11 ms (60%), Space: 20 MB (5.96%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,22 +2,21 @@ class Solution(object):
     def topKFrequent(self, nums, k):
         n = len(nums)
         hashmap = {}
+        for i in range(n):
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = 0
+            hashmap[nums[i]]+=1
         freq = [[] for _ in range(n+1)]
-        for n1 in nums:
-            if n1 not in hashmap:
-                hashmap[n1] = 1
-            else:
-                hashmap[n1]+=1
-        for element,ct in hashmap.items():
-            freq[ct].append(element)
+        for el,ct in hashmap.items():
+            freq[ct].append(el)
         res = []
-        for i in range(len(freq)-1,-1,-1):
-            if not freq[i]:
-                continue
-            for element in freq[i]:
-                res.append(element)
+        for i in range(n,-1,-1):
+            for el in freq[i]:
+                res.append(el)
                 if len(res)==k:
                     return res
 
-         
-            
+
+
+
+        
