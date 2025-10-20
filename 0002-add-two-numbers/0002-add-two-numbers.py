3324ed63320c0ff5e5f1dad4bd69efdbd4e7f4a2
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-10-20 17:37:12 -0500

    Time: 3 ms (90.65%), Space: 12.6 MB (54.49%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,28 +5,34 @@
 #         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        t1 = l1
-        t2 = l2
+        temp1 = l1
+        temp2 = l2
         dummynode = ListNode(-1)
-        curr = dummynode
+        temp = dummynode
         carry = 0
-        while(t1!=None or t2!=None):
-            sum1 = 0
-            sum1+=carry
-            if t1!=None:
-                sum1+=t1.val
-            if t2!=None:
-                sum1+=t2.val
-            newnode = ListNode(sum1%10)
-            carry = sum1/10
-            curr.next = newnode
-            curr = curr.next
-            if t1!=None:
-                t1 = t1.next
-            if t2!=None:
-                t2 = t2.next
-        if carry>0:
-            newnode = ListNode(carry)
-            curr.next = newnode
-            return dummynode.next
+        while(temp1!=None and temp2!=None):
+            sum1 = temp1.val + temp2.val + carry
+            remainder = sum1%10
+            carry = sum1//10
+            temp.next = ListNode(remainder)
+            temp1 = temp1.next
+            temp2 = temp2.next
+            temp = temp.next
+        while(temp1!=None):
+            sum1 = temp1.val+carry
+            remainder = sum1%10
+            carry = sum1//10
+            temp.next = ListNode(remainder)
+            temp1 = temp1.next
+            temp = temp.next
+        while(temp2!=None):
+            sum1 = temp2.val+carry
+            remainder = sum1%10
+            carry = sum1//10
+            temp.next = ListNode(remainder)
+            temp2 = temp2.next
+            temp = temp.next
+        if carry!=0:
+            temp.next = ListNode(carry)
+            
         return dummynode.next
