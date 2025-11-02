1448197e003f4d47e5f3f2518243f9addf52bfc1
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-11-01 19:28:52 -0500

    Time: 737 ms (48.54%), Space: 18.5 MB (27.07%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,18 +3,14 @@ class Solution(object):
         nums.sort()
         n = len(nums)
         ans = []
-        for i in range(n):
-            if i!=0 and nums[i]==nums[i-1]:
+        for i in range(len(nums)):
+            if i!=0 and nums[i-1]==nums[i]:
                 continue
             j = i+1
             k = n-1
             while(j<k):
                 sum1 = nums[i]+nums[j]+nums[k]
-                if sum1<0:
-                    j+=1
-                elif sum1>0:
-                    k-=1
-                else:
+                if sum1==0:
                     ans.append([nums[i],nums[j],nums[k]])
                     j+=1
                     k-=1
@@ -22,4 +18,8 @@ class Solution(object):
                         j+=1
                     while(j<k and nums[k]==nums[k+1]):
                         k-=1
+                elif sum1<0:
+                    j+=1
+                else:
+                    k-=1
         return ans
