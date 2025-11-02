24c3f6bd431ff9ab0b8736f6b2a3f4760cc63972
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-11-01 19:11:21 -0500

    Time: 247 ms (83.54%), Space: 78.6 MB (26.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,47 +1,54 @@
-class Node:
-    def __init__(self,key,value,next=None,prev= None):
+class ListNode(object):
+
+    def __init__(self, key,value):
         self.key = key
         self.value = value
-        self.next = next
-        self.prev = prev
+        self.next = None
+        self.prev = None
 class LRUCache(object):
+    def deletenode(self,temp):
+        back = temp.prev
+        front = temp.next
+        back.next = front
+        front.prev = back
+        temp.next = None
+        temp.prev = None
+    def insertafterhead(self,temp):
+        front = self.head.next
+        self.head.next = temp
+        temp.next = front
+        front.prev = temp
+        temp.prev = self.head
     def __init__(self, capacity):
         self.capacity = capacity
-        self.head = Node(-1,-1)
-        self.tail = Node(-1,-1)
+        self.hashmap = {}
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.mapp = {}
-    def deletenode(self,node):
-        prevnode = node.prev
-        nextnode = node.next
-        prevnode.next = nextnode
-        nextnode.prev = prevnode
-    def insertafterhead(self,node):
-        currafterhead = self.head.next
-        self.head.next = node
-        currafterhead.prev = node
-        node.next = currafterhead
-        node.prev = self.head
     def get(self, key):
-        if key not in self.mapp:
+        if key not in self.hashmap:
             return -1
-        else:
-            temp = self.mapp[key]
-            self.deletenode(temp)
-            self.insertafterhead(temp)
-        return temp.value
+        node = self.hashmap[key]
+        self.deletenode(node)
+        self.insertafterhead(node)
+        return node.value
     def put(self, key, value):
-        if key in self.mapp:
-            temp = self.mapp[key]
-            temp.value = value
-            self.deletenode(temp)
-            self.insertafterhead(temp)
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            node.value = value
+            self.deletenode(node)
+            self.insertafterhead(node)
         else:
-            if len(self.mapp)>=self.capacity:
-                temp = self.tail.prev
-                self.mapp.pop(temp.key)
-                self.deletenode(temp)
-            newnode = Node(key,value)
-            self.mapp[key] = newnode
+            if self.capacity==len(self.hashmap):
+                node = self.tail.prev
+                self.deletenode(node)
+                del self.hashmap[node.key]
+            newnode = ListNode(key,value)
+            self.hashmap[key] = newnode
             self.insertafterhead(newnode)
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
