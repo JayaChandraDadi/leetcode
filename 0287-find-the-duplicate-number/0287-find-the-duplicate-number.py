3145b73261aa4c3089b878fabd15ba4959680dc5
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-11-03 23:38:27 -0600

    Time: 44 ms (35.01%), Space: 20.8 MB (75.29%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,9 +1,12 @@
 class Solution(object):
     def findDuplicate(self, nums):
-        hashmap = {}
-        for i in range(len(nums)):
-            if nums[i] in hashmap:
-                return nums[i]
-            hashmap[nums[i]] = 1
-        
-        
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while(slow!=fast):
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        fast = 0
+        while(fast!=slow):
+            fast = nums[fast]
+            slow = nums[slow]
+        return slow
