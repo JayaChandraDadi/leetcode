cfb213735572765e47f2a50f548d0c1b5593c053
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-11-05 17:18:09 -0600

    Time: 3599 ms (93.67%), Space: 12.5 MB (52.73%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,40 +1,42 @@
 class Solution(object):
-    def analyze(self,row,col,board,word,i,n,vis,m):
+    def findboard(self,board,row,col,i,vis,m,n,word):
         if i==len(word):
             return True
-        if row+1<m and board[row+1][col]==word[i] and vis[row+1][col]!=1:
-            vis[row+1][col] = 1
-            if self.analyze(row+1,col,board,word,i+1,n,vis,m):
-                return True
-            vis[row+1][col] = 0
-        if col-1>=0 and board[row][col-1]==word[i] and vis[row][col-1]!=1:
-            vis[row][col-1] = 1
-            if self.analyze(row,col-1,board,word,i+1,n,vis,m):
-                return True
-            vis[row][col-1] = 0
-        if col+1<n and board[row][col+1]==word[i] and vis[row][col+1]!=1:
-            vis[row][col+1] = 1
-            if self.analyze(row,col+1,board,word,i+1,n,vis,m):
-                return True
-            vis[row][col+1] = 0
-        if row-1>=0 and board[row-1][col]==word[i] and vis[row-1][col]!=1:
-            vis[row-1][col] = 1
-            if self.analyze(row-1,col,board,word,i+1,n,vis,m):
-                return True
-            vis[row-1][col] = 0
+        if row+1<m:
+            if word[i]==board[row+1][col] and vis[row+1][col]==0:
+                vis[row+1][col] = 1
+                if self.findboard(board,row+1,col,i+1,vis,m,n,word):
+                    return True
+                vis[row+1][col] = 0
+        if col+1<n:
+            if word[i]==board[row][col+1] and vis[row][col+1]==0:
+                vis[row][col+1] = 1
+                if self.findboard(board,row,col+1,i+1,vis,m,n,word):
+                    return True
+                vis[row][col+1] = 0
+        if row-1>=0:
+            if word[i]==board[row-1][col] and vis[row-1][col]==0:
+                vis[row-1][col] = 1
+                if self.findboard(board,row-1,col,i+1,vis,m,n,word):
+                    return True
+                vis[row-1][col] = 0
+        if col-1>=0:
+            if word[i]==board[row][col-1] and vis[row][col-1]==0:
+                vis[row][col-1] = 1
+                if self.findboard(board,row,col-1,i+1,vis,m,n,word):
+                    return True
+                vis[row][col-1] = 0
+        return False
+    def exist(self, board, word):
+        m = len(board)
+        n = len(board[0]) if m else 0
+        vis = [[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]==word[0]:
+                    vis[i][j] = 1
+                    if self.findboard(board,i,j,1,vis,m,n,word):
+                        return True
+                    vis[i][j] = 0
         return False
         
-    def exist(self, board, word):
-        word = list(word)
-        m = len(board)
-        n = len(board[0])
-        vis = [[0]*n for _ in range(m)]
-        for col in range(n):
-            for row in range(m):
-                if board[row][col]==word[0]:
-                    vis[row][col] = 1
-                    if self.analyze(row,col,board,word,1,n,vis,m):
-                        return True
-                    vis[row][col] = 0
-        return False
-
