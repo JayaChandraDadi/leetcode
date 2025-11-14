f6787597c62b9e4157a8eb8ad7f9d66425be064c
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-11-14 00:36:35 -0600

    Time: 519 ms (41.85%), Space: 12.6 MB (25.14%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,28 +1,29 @@
 class Solution(object):
     def fourSum(self, nums, target):
-        n = len(nums)
-        nums.sort()
-        ans = []
-        for i in range(n):
-            if i!=0 and nums[i-1]==nums[i]:
+       nums.sort()
+       n = len(nums)
+       ans = []
+       for i in range(n):
+        if i>0 and nums[i-1]==nums[i]:
+            continue
+        for j in range(i+1,n):
+            if j>i+1 and nums[j-1]==nums[j]:
                 continue
-            for j in range(i+1,n):
-                if j!=i+1 and nums[j-1]==nums[j]:
-                    continue
-                k = j+1
-                l = n-1
-                while(k<l):
-                    sum1 = nums[i]+nums[j]+nums[k]+nums[l]
-                    if sum1<target:
+            k = j+1
+            l = n-1
+            while(k<l):
+                sum1 = nums[i]+nums[j]+nums[k]+nums[l]
+                if sum1==target:
+                    ans.append([nums[i],nums[j],nums[k],nums[l]])
+                    while(k<l and nums[k+1]==nums[k]):
                         k+=1
-                    elif sum1>target:
+                    while(k<l and nums[l]==nums[l-1]):
                         l-=1
-                    else:
-                        ans.append([nums[i],nums[j],nums[k],nums[l]])
-                        k+=1
-                        l-=1
-                        while k<l and nums[k]==nums[k-1]:
-                            k+=1
-                        while k<l and nums[l]==nums[l+1]:
-                            l-=1
-        return ans
+                    k+=1
+                    l-=1
+                elif sum1>target:
+                    l-=1
+                else:
+                    k+=1
+                    
+       return ans
