11f5bcc5ac529a7d3aa25dea329a1cdc6d4abe65
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2025-11-26 02:18:18 -0500

    Time: 104 ms (5.49%), Space: 13.4 MB (54.23%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def check(self,temp1,temp2):
+        if temp1==None and temp2==None:
+            return True
+        if temp1==None or temp2==None:
+            return False
+        if temp1.val!=temp2.val:
+            return False
+        return self.check(temp1.left,temp2.left) and self.check(temp1.right,temp2.right)
+    def preorder(self,root,subroot):
+        if not root:
+            return False
+        if root.val==subroot.val and self.check(root,subroot):
+            return True
+        leftsubtree = self.preorder(root.left,subroot)
+        rightsubtree = self.preorder(root.right,subroot)
+        return leftsubtree or rightsubtree
+    def isSubtree(self, root, subRoot):
+        return self.preorder(root,subRoot)
