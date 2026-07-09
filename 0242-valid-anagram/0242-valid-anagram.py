cd2511b29dc3b617b3a2dc23ed1ba3f9f93a866e
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-08 20:20:39 -0400

    Time: 11 ms (77.12%), Space: 19.4 MB (75.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_map = {}
-        t_map = {}
+        s_map = [0]*(26)
+        t_map = [0]*(26)
         if len(t)!=len(s):
             return False
         for i in range(len(t)):
-            if s[i] not in s_map:
-                s_map[s[i]] = 0
-            s_map[s[i]]+=1
-            if t[i] not in t_map:
-                t_map[t[i]] = 0
-            t_map[t[i]]+=1
-        for ch,freq in s_map.items():
-            if ch not in t_map or freq!=t_map[ch]:
+            s_map[ord(s[i]) - ord('a')]+=1
+            t_map[ord(t[i]) - ord('a')]+=1
+        for i in range(26):
+            if s_map[i]!=t_map[i]:
                 return False
         return True
