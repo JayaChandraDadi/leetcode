f323e138d4c1528a7202f0a7abe50ccd217d1c44
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-08 22:56:22 -0400

    Time: 7 ms (28.93%), Space: 19.2 MB (73.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,15 +1,15 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        rows = defaultdict(set) 
-        cols = defaultdict(set)
-        squares = defaultdict(set)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_map = defaultdict(set)
+        col_map = defaultdict(set)
+        box_map = defaultdict(set)
         for i in range(9):
             for j in range(9):
-                if board[i][j]==".":
+                if board[i][j]=='.':
                     continue
-                if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in squares[(i//3,j//3)]:
+                if board[i][j] in row_map[i] or board[i][j] in col_map[j] or board[i][j] in box_map[(i//3,j//3)]:
                     return False
-                rows[i].add(board[i][j])
-                cols[j].add(board[i][j])
-                squares[(i//3,j//3)].add(board[i][j])
+                row_map[i].add(board[i][j])
+                col_map[j].add(board[i][j])
+                box_map[(i//3,j//3)].add(board[i][j])
         return True
