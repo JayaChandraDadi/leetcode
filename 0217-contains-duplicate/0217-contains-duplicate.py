dd8e7e7bcd530aaf281ebfb704ed26d03671e85f
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-08 20:14:49 -0400

    Time: 23 ms (19.48%), Space: 37.4 MB (5.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,10 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        if len(nums)==len(set(nums)):
-            return False
-        return True
-        
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        hashmap = {}
+        for i in range(n):
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = 1
+            else:
+                return True
+        return False
