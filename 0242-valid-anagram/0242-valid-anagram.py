b0734b15e09c055662d4ade1434fb2215c211bee
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-08 20:18:23 -0400

    Time: 15 ms (44.32%), Space: 19.5 MB (29.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,17 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        s_sorted = sorted(s)
-        t_sorted = sorted(t)
-        if len(s)!=len(t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+        if len(t)!=len(s):
             return False
-        i = 0
-        j = 0
-        while(i<len(s) and j<len(t)):
-            if s_sorted[i]==t_sorted[j]:
-                i+=1
-                j+=1
-            else:
+        for i in range(len(t)):
+            if s[i] not in s_map:
+                s_map[s[i]] = 0
+            s_map[s[i]]+=1
+            if t[i] not in t_map:
+                t_map[t[i]] = 0
+            t_map[t[i]]+=1
+        for ch,freq in s_map.items():
+            if ch not in t_map or freq!=t_map[ch]:
                 return False
         return True
-        
