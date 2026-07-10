16d0c31e7cffc1b5718b0c40543e37909bbeb517
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-09 20:52:56 -0400

    Time: 8 ms (49.35%), Space: 23.3 MB (11.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def isPalindrome(self, s):
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ''.join(c.lower() for c in s if c.isalnum())
         i = 0
-        string = ''.join(c for c in s if c.isalnum()).lower()
-        j = len(string)-1
-        while(i<=j):
-            if string[i]!=string[j]:
+        j = len(res) - 1
+        while(i<j):
+            if res[i]!=res[j]:
                 return False
-            else:
-                i+=1
-                j-=1
+            i+=1
+            j-=1
         return True
-        
