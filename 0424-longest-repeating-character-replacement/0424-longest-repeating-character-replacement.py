fef988b596d84fe0a6ab9780d3a257822519c302
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-14 17:58:16 -0400

    Time: 91 ms (47.94%), Space: 19.7 MB (14.69%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,19 +1,20 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-       n = len(s)
-       maxlen = 0
-       maxfreq = 0
-       l = 0
-       r = 0
-       hashmap = [0]*26
-       while(r<n):
-        hashmap[ord(s[r])-ord('A')]+=1
-        maxfreq = max(maxfreq,hashmap[ord(s[r])-ord('A')])
-        changes = r-l+1-maxfreq
-        if changes<=k:
-            maxlen = max(maxlen,r-l+1)
-        else:
-            hashmap[ord(s[l])-ord('A')]-=1
-            l+=1
-        r+=1
-       return maxlen
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        r = 0
+        maxfreq = 0
+        hashmap = {}
+        maxlen = float('-inf')
+        while(r<len(s)):
+            if s[r] not in hashmap:
+                hashmap[s[r]] = 0
+            hashmap[s[r]]+=1
+            maxfreq = max(maxfreq,hashmap[s[r]])
+            changes = r-l+1-maxfreq
+            if changes>k:
+                hashmap[s[l]]-=1
+                l+=1
+            else:
+                maxlen = max(maxlen,r-l+1)
+            r+=1
+        return maxlen
