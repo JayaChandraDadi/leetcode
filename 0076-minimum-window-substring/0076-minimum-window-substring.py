46be796a60ca73a1615ac8af14b1edf4e8b285c5
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-15 11:27:49 -0400

    Time: 122 ms (33.58%), Space: 19.7 MB (73.41%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,36 +1,29 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        need = [0] * 256
-        window = [0] * 256
-
-        for c in t:
-            need[ord(c)] += 1
-
-        required = sum(1 for x in need if x > 0)
-        formed = 0
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        freq = {}
+        for i in range(len(t)):
+            if t[i] not in freq:
+                freq[t[i]] = 0
+            freq[t[i]]+=1
         l = 0
         r = 0
-        min_len = float('inf')
-        start = 0
-
-        while r < len(s):
-            char_r = ord(s[r])
-            window[char_r] += 1
-
-            if need[char_r] > 0 and window[char_r] == need[char_r]:
-                formed += 1
-
-            while l <= r and formed == required:
-                if r - l + 1 < min_len:
-                    min_len = r - l + 1
-                    start = l
-
-                char_l = ord(s[l])
-                window[char_l] -= 1
-                if need[char_l] > 0 and window[char_l] < need[char_l]:
-                    formed -= 1
-                l += 1
-
-            r += 1
-
-        return "" if min_len == float('inf') else s[start:start + min_len]
+        ct = len(t)
+        minlength = float('inf')
+        minstring = ""
+        while(r<len(s)):
+            if s[r] in freq:
+                if freq[s[r]]>0:
+                    ct-=1
+                freq[s[r]]-=1
+            while(ct==0):
+                if r-l+1<minlength:
+                    minlength = r-l+1
+                    minstring = s[l:r+1]
+                if s[l] in freq:
+                    freq[s[l]]+=1
+                    if freq[s[l]]>0:
+                        ct+=1
+                l+=1
+            r+=1
+        return minstring
+                
