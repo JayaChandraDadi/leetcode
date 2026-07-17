1a5179b0f99a2e6ec7e765429a88e067e3d03aaf
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-17 10:22:31 -0400

    Time: 128 ms (10.08%), Space: 32.8 MB (13.24%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,30 +1,31 @@
-class MinStack(object):
+class MinStack:
+
     def __init__(self):
         self.st = []
-        self.mini = None
-    
-    def push(self, val):
-        self.st.append(val)
-        if self.mini is None or val < self.mini:
-            self.mini = val
-        elif val >= self.mini:
-            pass  # No encoding needed unless val < mini
-    
-    def pop(self):
+    def push(self, value: int) -> None:
+        if not self.st:
+            self.st.append([value,value])
+            return
+        current_min1 = self.st[-1][1]
+        updated_min1 = min(current_min1,value)
+        self.st.append([value,updated_min1]) 
+    def pop(self) -> None:
+        if not self.st:
+            return 
+        self.st.pop()
+    def top(self) -> int:
+        if not self.st:
+            return 
+        return self.st[-1][0]
+
+    def getMin(self) -> int:
         if not self.st:
             return
-        x = self.st.pop()
-        if not self.st:  # Stack will be empty
-            self.mini = None
-        elif x == self.mini:  # Recalculate minimum
-            self.mini = min(self.st)
-    
-    def top(self):
-        if not self.st:
-            return None
-        return self.st[-1]
-    
-    def getMin(self):
-        if not self.st:
-            return None
-        return self.mini
+        return self.st[-1][1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
