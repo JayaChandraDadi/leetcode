88c3f5785f81567b0411288f2df3d0ffac63934f
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-20 15:19:31 -0400

    Time: 43 ms (5.05%), Space: 29.2 MB (5.05%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,23 +1,14 @@
 class Solution:
-    def profit(self,i,buy,prices,dp):
+    def dfs(self,i,buy,prices,dp):
         if i==len(prices):
             return 0
         if dp[i][buy]!=-1:
             return dp[i][buy]
         if buy==1:
-            dp[i][buy] = max(-prices[i] + self.profit(i+1,0,prices,dp),self.profit(i+1,1,prices,dp))
+            dp[i][buy] = max(-prices[i] + self.dfs(i+1,0,prices,dp),self.dfs(i+1,1,prices,dp))
         else:
-            dp[i][buy] = max(prices[i] + self.profit(i+1,1,prices,dp),self.profit(i+1,0,prices,dp))
+            dp[i][buy] = max(prices[i]+self.dfs(i+1,1,prices,dp),self.dfs(i+1,0,prices,dp))
         return dp[i][buy]
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        prev = [0]*(2)
-        for i in range(len(prices)-1,-1,-1):
-            curr = [0]*(2)
-            for buy in range(2):
-                if buy==1:
-                    curr[buy] = max(-prices[i] + prev[0],prev[1])
-                else:
-                    curr[buy] = max(prices[i] + prev[1],prev[0])
-            prev = curr
-        return prev[1]
+        dp = [[-1]*(2) for _ in range(len(prices))]
+        return self.dfs(0,1,prices,dp)
