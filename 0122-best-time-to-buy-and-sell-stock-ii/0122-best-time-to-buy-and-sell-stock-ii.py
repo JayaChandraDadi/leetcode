8983a108e2d059d3cdc722ab4fcb7de37b7d7f29
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-20 15:07:08 -0400

    Time: 8 ms (11.1%), Space: 20.5 MB (10.34%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,27 +1,23 @@
-class Solution(object):
-    def profit(self,i,buy,n,prices,dp):
-        if i==n:
+class Solution:
+    def profit(self,i,buy,prices,dp):
+        if i==len(prices):
             return 0
         if dp[i][buy]!=-1:
             return dp[i][buy]
         if buy==1:
-            dp[i][buy] = max(-prices[i]+self.profit(i+1,0,n,prices,dp),self.profit(i+1,1,n,prices,dp))
+            dp[i][buy] = max(-prices[i] + self.profit(i+1,0,prices,dp),self.profit(i+1,1,prices,dp))
         else:
-            dp[i][buy] = max(prices[i]+self.profit(i+1,1,n,prices,dp),self.profit(i+1,0,n,prices,dp))
+            dp[i][buy] = max(prices[i] + self.profit(i+1,1,prices,dp),self.profit(i+1,0,prices,dp))
         return dp[i][buy]
-    def maxProfit(self, prices):
+    def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        prev = [0]*2 
-        temp = [0]*2
-        for i in range(n-1,-1,-1):
+        prev = [0]*(2)
+        for i in range(len(prices)-1,-1,-1):
+            curr = [0]*(2)
             for buy in range(2):
-                profit = 0
                 if buy==1:
-                    profit = max(-prices[i]+prev[0],prev[1])
+                    curr[buy] = max(-prices[i] + prev[0],prev[1])
                 else:
-                    profit = max(prices[i]+prev[1],prev[0])
-                temp[buy] = profit
-            prev = temp
-        return profit
-
-        
+                    curr[buy] = max(prices[i] + prev[1],prev[0])
+            prev = curr
+        return prev[1]
