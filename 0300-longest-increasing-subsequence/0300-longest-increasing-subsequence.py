7e85ea4afe04d528f4c2a666020720644182d02e
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-20 19:42:30 -0400

    Time: 7 ms (83.14%), Space: 19.5 MB (31.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,34 +1,21 @@
-class Solution(object):
-    def length(self,i,prev,nums,n,dp):
-        if i==n:
-            return 0
-        take =float('-inf')
-        if dp[i][prev+1]!=-1:
-            return dp[i][prev+1]
-        if prev==-1 or nums[prev]<nums[i]:
-            take = 1+self.length(i+1,i,nums,n,dp)
-        nottake = self.length(i+1,prev,nums,n,dp)
-        dp[i][prev+1] =  max(take,nottake)
-        return dp[i][prev+1]
-    def lowerbound(self,low,high,el,arr):
+class Solution:
+    def ceil(self,nums,target):
+        low = 0
+        high = len(nums)
         while(low<=high):
-            mid = (low+high)//2
-            if arr[mid]<el:
-                low = mid+1
+            mid = (low + high)//2
+            if nums[mid]<target:
+                low = mid + 1
             else:
-                index = mid
-                high = mid-1
-        return index
-    def lengthOfLIS(self, nums):
-        n = len(nums)
+                high = mid - 1
+        return low
+    def lengthOfLIS(self, nums: List[int]) -> int:
         temp = []
-        temp.append(nums[0])
-        length = 1
-        for i in range(1,n):
-            if temp[-1]<nums[i]:
+        n = len(nums)
+        for i in range(n):
+            if not temp or temp[-1]<nums[i]:
                 temp.append(nums[i])
-                length+=1
             else:
-                index = self.lowerbound(0,len(temp)-1,nums[i],temp)
+                index = self.ceil(temp,nums[i])
                 temp[index] = nums[i]
-        return length
+        return len(temp)
