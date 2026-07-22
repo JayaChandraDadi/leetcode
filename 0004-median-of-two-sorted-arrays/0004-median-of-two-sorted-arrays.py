37147741073f04f6ea19f0a07a2dbcf0a635a53e
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-22 15:25:21 -0400

    Time: 7 ms (12.85%), Space: 19.6 MB (41.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,33 +1,25 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if len(nums1)>len(nums2):
             nums1,nums2 = nums2,nums1
         n1 = len(nums1)
         n2 = len(nums2)
+        n = n1 + n2
         low = 0
-        left = (n1+n2+1)//2
         high = n1
-        n = n1+n2
         while(low<=high):
-            mid1 = (low+high)//2
-            mid2 = left-mid1
-            r1 = nums1[mid1] if mid1<n1 else float("inf")
-            r2 = nums2[mid2] if mid2<n2 else float("inf")
-            l1 = nums1[mid1-1] if mid1-1>=0 else float("-inf")
-            l2 = nums2[mid2-1] if mid2-1>=0 else float("-inf")
-            if l1<=r2 and l2<=r1:
+            mid1 = (low+ high)//2
+            mid2 = (n+1)//2 - mid1
+            l1 = nums1[mid1-1] if mid1>0 else float('-inf')
+            l2 = nums2[mid2-1] if mid2>0 else float('-inf')
+            r1 = nums1[mid1] if mid1<n1 else float('inf')
+            r2 = nums2[mid2] if mid2<n2 else float('inf')
+            if l1>r2:
+                high = mid1 - 1
+            elif r1<l2:
+                low = mid1+1
+            else:
                 if n%2==0:
-                    return (max(l1,l2)+min(r1,r2))/2.0
+                    return (max(l1,l2)+min(r1,r2))/2
                 else:
                     return max(l1,l2)
-            elif l1>r2:
-                high = mid1-1
-            else:
-                low = mid1+1
-        return -1
-
-            
-
-
-
-        
