589a1f376f9ee82d0a21b3270526a255baf85e1d
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-27 00:48:44 -0400

    Time: 0 ms (100%), Space: 20.6 MB (97.61%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -1,21 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def inorder(self,root):
+class Solution:
+    def dfs(self,root):
         if not root:
             return 
-        self.inorder(root.left)
-        self.ans.append(root.val)
-        self.inorder(root.right)
-    def getMinimumDifference(self, root):
-        min1 = float('inf')
-        self.ans = []
-        self.inorder(root)
-        for i in range(1,len(self.ans)):
-            min1 = min(min1,abs(self.ans[i]-self.ans[i-1]))
-        return min1
-        
+        self.dfs(root.left)
+        self.inorder.append(root.val)
+        self.dfs(root.right)
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.inorder = []
+        minlen = float('inf')
+        self.dfs(root)
+        for i in range(1,len(self.inorder)):
+            minlen = min(minlen,abs(self.inorder[i] - self.inorder[i-1]))
+        return minlen
