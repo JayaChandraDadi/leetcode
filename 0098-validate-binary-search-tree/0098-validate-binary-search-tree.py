4d715e9ac10044893df22dcdaa60e3143f473163
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-27 00:49:05 -0400

    Time: 0 ms (100%), Space: 20.8 MB (73.71%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,16 +1,15 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-class Solution(object):
-    def preorder(self,root,min1,max1):
+class Solution:
+    def validate(self,root,min1,max1):
         if not root:
             return True
-        if min1>=root.val or root.val>=max1:
+        if min1>=root.val or max1<=root.val:
             return False
-        return self.preorder(root.left,min1,root.val) and self.preorder(root.right,root.val,max1)  
-    def isValidBST(self, root):
-        return self.preorder(root,float('-inf'),float('inf'))
+        return self.validate(root.left,min1,root.val) and self.validate(root.right,root.val,max1)
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.validate(root,float('-inf'),float('inf'))
