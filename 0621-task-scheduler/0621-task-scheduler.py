5e2d58c85dc7eb2a9f0c1dfc8cbcdc986649a1e7
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-07-27 23:11:26 -0400

    Time: 43 ms (68.84%), Space: 20.7 MB (64.39%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -2,28 +2,20 @@ import heapq
 from collections import deque
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        pq = []
+        maxfreq = 0
         freq = {}
-        q = deque()
         for i in range(len(tasks)):
             if tasks[i] not in freq:
                 freq[tasks[i]] = 0
             freq[tasks[i]]+=1
+            maxfreq = max(maxfreq,freq[tasks[i]])
+        maxct = 0
         for task,ct in freq.items():
-            heapq.heappush(pq,(-ct,task))
-        time = 1
-        while(pq or q):
-            while q and time>=q[0][0]:
-                _,task,ct = q.popleft()
-                heapq.heappush(pq,(-ct,task))
-            if pq:
-                ct,task = heapq.heappop(pq)
-                ct = -ct
-                ct = ct-1
-                if ct>0:
-                    q.append((time+n+1,task,ct))
-                time+=1
-            else:
-                while time<q[0][0]:
-                    time+=1
-        return time-1
+            if ct==maxfreq:
+                maxct+=1
+            partation_size = n-(maxct-1)
+            partations = maxfreq - 1
+            empty_slots = partations*partation_size
+            remaining_tasks = len(tasks) - maxfreq*maxct
+            idlesots = max(0,empty_slots-remaining_tasks)
+        return len(tasks) + idlesots
