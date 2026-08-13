5d6d426a56b2b0e15dd454b0ba8bff88a2f88571
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-08-13 19:34:43 -0400

    Time: 107 ms (31.16%), Space: 20 MB (39.93%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def dfs(self,r,c,visited,drc,grid,m,n):
         visited[r][c] = True
@@ -15,9 +16,27 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0]) if m else 0
+        q = deque()
+        ans = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==1:
                     visited = [[False]*n for _ in range(m)]
                     drc = [(-1,0),(0,1),(1,0),(0,-1)]
-                    return self.dfs(i,j,visited,drc,grid,m,n)
+                    q.append((i,j))
+                    visited[i][j] = True
+                    while(q):
+                        r,c = q.popleft()
+                        perimeter = 0
+                        for dr,dc in drc:
+                            nr = r + dr
+                            nc = c + dc
+                            if nr>=0 and nr<m and nc>=0 and nc<n and visited[nr][nc]==False and grid[nr][nc]==1:
+                                q.append((nr,nc))
+                                visited[nr][nc] = True
+                            elif nr>=0 and nr<m and nc>=0 and nc<n and grid[nr][nc]==1 and visited[nr][nc]==True:
+                                continue
+                            else:
+                                perimeter+=1
+                        ans+=perimeter
+                    return ans
