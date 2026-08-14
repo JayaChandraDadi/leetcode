355f11bd74287db882b44b19d9d6c0f2f6e5b9f3
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-08-13 23:22:17 -0500

    Time: 242 ms (61.47%), Space: 21.5 MB (91.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def dfs(self,r,c,grid,visited,m,n,drc):
         visited[r][c] = True
@@ -11,10 +12,20 @@ class Solution:
         n = len(grid[0]) if m else 0
         visited = [[False]*n for _ in range(m)]
         ct = 0
+        q = deque()
         drc = [(-1,0),(0,1),(1,0),(0,-1)]
         for i in range(m):
             for j in range(n):
                 if grid[i][j]=='1' and visited[i][j]==False:
                     ct+=1
-                    self.dfs(i,j,grid,visited,m,n,drc)
+                    visited[i][j] = True
+                    q.append((i,j))
+                    while(q):
+                        r,c = q.popleft()
+                        for dr,dc in drc:
+                            nr = r + dr
+                            nc = c + dc
+                            if nr>=0 and nr<m and nc>=0 and nc<n and visited[nr][nc]==False and grid[nr][nc]=='1':
+                                q.append((nr,nc))
+                                visited[nr][nc] = True
         return ct
