0ef5ae6c6f08e1b09a90eb80c11d5933207e033f
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-08-17 19:28:59 -0500

    Time: 1 ms (91.11%), Space: 20.7 MB (49.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,30 +1,25 @@
-from collections import deque
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        indegree = [0]*numCourses
-        pathvis = [0]*numCourses
-        topo = []
-        queue = deque()
-        for i in range(numCourses):
-            adj = [[] for _ in range(numCourses)]
+class Solution:
+    def dfs(self,node,adj,visited,pathvisited):
+        visited[node] = True
+        pathvisited[node] = True
+        for nei in adj[node]:
+            if visited[nei]==True and pathvisited[nei]==True:
+                return False
+            elif visited[nei]==True and pathvisited[nei]==False:
+                continue
+            else:
+                if self.dfs(nei,adj,visited,pathvisited)==False:
+                    return False
+        pathvisited[node] = False
+        return True
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[]*numCourses for _ in range(numCourses)]
         for u,v in prerequisites:
-            adj[v].append(u)
-        for i in range(numCourses):
-            for neighbour in adj[i]:
-                indegree[neighbour]+=1
-        for i in range(numCourses):
-            if indegree[i]==0:
-                queue.append(i)
-                topo.append(i)
-        while(len(queue)!=0):
-            node = queue.popleft()
-            for neighbour in adj[node]:
-                indegree[neighbour]-=1
-                if indegree[neighbour]==0:
-                    queue.append(neighbour)
-                    topo.append(neighbour)
-        if len(topo)==numCourses:
-            return True
-        return False
-
-            
+            adj[u].append(v)
+        visited = [False]*numCourses
+        pathvisited = [False]*numCourses
+        for node in range(numCourses):
+            if not visited[node]:
+                if self.dfs(node,adj,visited,pathvisited)==False:
+                    return False
+        return True
