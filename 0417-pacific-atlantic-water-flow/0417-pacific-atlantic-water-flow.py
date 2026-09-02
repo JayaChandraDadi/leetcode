bc778d7ecd432ff564d4e0118ac8a90a10ff92d2
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-09-02 16:18:57 -0500

    Time: 27 ms (74.17%), Space: 20.7 MB (87.99%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,29 +1,28 @@
 class Solution:
-    def dfs(self,grid,r,c,visited,drc,m,n):
+    def dfs(self,r,c,visited,heights,drc,m,n):
         visited[r][c] = True
         for dr,dc in drc:
             nr = r + dr
             nc = c + dc
-            if nr>=0 and nr<m and nc>=0 and nc<n and grid[nr][nc]>=grid[r][c] and visited[nr][nc]==False:
-                self.dfs(grid,nr,nc,visited,drc,m,n)
-        return 
+            if nr>=0 and nr<m and nc>=0 and nc<n and heights[nr][nc]>=heights[r][c] and visited[nr][nc]==False:
+                self.dfs(nr,nc,visited,heights,drc,m,n)
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m = len(heights)
         n = len(heights[0]) if m else 0
-        pacific = [[False]*n for _ in range(m)]
+        pacific_visited = [[False]*n for _ in range(m)]
+        atlantic_visited = [[False]*n for _ in range(m)]
+        ans = []
         drc = [(-1,0),(0,1),(1,0),(0,-1)]
         for i in range(m):
-            self.dfs(heights,i,0,pacific,drc,m,n)
+            self.dfs(i,0,pacific_visited,heights,drc,m,n)
         for j in range(n):
-            self.dfs(heights,0,j,pacific,drc,m,n)
-        atlantic = [[False]*n for _ in range(m)]
+            self.dfs(0,j,pacific_visited,heights,drc,m,n)
         for i in range(m):
-            self.dfs(heights,i,n-1,atlantic,drc,m,n)
+            self.dfs(i,n-1,atlantic_visited,heights,drc,m,n)
         for j in range(n):
-            self.dfs(heights,m-1,j,atlantic,drc,m,n)
-        ans = []
+            self.dfs(m-1,j,atlantic_visited,heights,drc,m,n)
         for i in range(m):
             for j in range(n):
-                if pacific[i][j]==True and atlantic[i][j]==True:
+                if atlantic_visited[i][j]==True and pacific_visited[i][j]==True:
                     ans.append([i,j])
         return ans
