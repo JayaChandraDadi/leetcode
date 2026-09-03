98d11faadfb4a2b9a971af9b3f9a87a01afe16d0
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-09-03 16:58:59 -0500

    Time: 39 ms (32.15%), Space: 22.4 MB (57.06%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,48 +1,47 @@
-class Solution(object):
-    def findpar(self,node,parent):
-        if parent[node]==node:
-            return node
-        parent[node] = self.findpar(parent[node],parent)
-        return parent[node]
-    def unionbyrank(self,u,v,rank,parent):
+class Solution:
+    def findpar(self,x,parent):
+        if x!=parent[x]:
+            parent[x] = self.findpar(parent[x],parent)
+        return parent[x]
+    def union(self,u,v,parent,size):
         pu = self.findpar(u,parent)
         pv = self.findpar(v,parent)
-        if rank[pu]<rank[pv]:
+        if pu==pv:
+            return 
+        if size[pu]<size[pv]:
+            size[pv]+=size[pu]
             parent[pu] = pv
-        elif rank[pv]<rank[pu]:
+        elif size[pv]<size[pu]:
+            size[pu]+=size[pv]
             parent[pv] = pu
         else:
-            parent[pu] = pv
-            rank[pv]+=1
-    def accountsMerge(self, accounts):
-        n = len(accounts)
-        parent = [0]*n
-        rank = [0]*n
-        for i in range(n):
-            parent[i] = i
-        mapmailnode = {}
-        for i in range(n):
-            for j in range(1,len(accounts[i])):
-                mail = accounts[i][j]
-                if mail not in mapmailnode:
-                    mapmailnode[mail] = i
-                else:
-                    self.unionbyrank(i,mapmailnode[mail],rank,parent)
-        mergedmail = [[]for _ in range(n)]
-        for mail,index in mapmailnode.items():
-            node = self.findpar(index,parent)
-            mergedmail[node].append(mail)
+            size[pu]+=size[pv]
+            parent[pv] = pu
+        return 
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        parent = {}
+        size = {}
+        email_to_name = {}
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                email_to_name[email] = name
+                parent[email] = email
+                size[email] = 1
+        for account in accounts:
+            name = account[0]
+            email1 = account[1]
+            for email2 in account[2:]:
+                self.union(email1,email2,parent,size)
+        components = {}
+        for email in parent:
+            root = self.findpar(email,parent)
+            if root not in components:
+                components[root] = []
+            components[root].append(email)
         ans = []
-        for i in range(n):
-            if len(mergedmail[i])==0:
-                continue
-            mergedmail[i].sort()
-            temp = []
-            temp.append(accounts[i][0])
-            for it in mergedmail[i]:
-                temp.append(it)
-            ans.append(temp)
+        for root in components:
+            name = email_to_name[root]
+            emails = sorted(components[root])
+            ans.append([name] + emails)
         return ans
-
-        
-        
