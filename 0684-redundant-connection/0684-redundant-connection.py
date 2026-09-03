ab05384eac08d09efd9135a6e8ad39e0243cfc42
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-09-03 16:27:22 -0500

    Time: 0 ms (100%), Space: 19.5 MB (95.86%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,36 +1,29 @@
 class Solution:
-    def dfs(self,node,par,adj,visited,parent):
-        visited[node] = True
-        for nei in adj[node]:
-            if visited[nei]==True and nei!=par:
-                if self.cycle_start==-1:
-                    self.cycle_start = nei
-                parent[nei] = node
-                return True
-            elif visited[nei]==False:
-                parent[nei] = node
-                if self.dfs(nei,node,adj,visited,parent)==True:
-                    return True
-        return False
+    def findpar(self,x,parent):
+        if x!=parent[x]:
+            parent[x] = self.findpar(parent[x],parent)
+        return parent[x]
+    def union(self,u,v,parent,size):
+        pu = self.findpar(u,parent)
+        pv = self.findpar(v,parent)
+        if pu==pv:
+            return False
+        if size[pu]<size[pv]:
+            size[pv]+=size[pu]
+            parent[pu] = pv
+        elif size[pv]<size[pu]:
+            size[pu]+=size[pv]
+            parent[pv] = pu
+        else:
+            size[pu]+=size[pv]
+            parent[pv] = pu
+        return True
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
-        self.cycle_start = -1
         parent = [0]*(n+1)
-        adj = [[] for _ in range(n+1)]
-        for u,v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
         for i in range(n+1):
             parent[i] = i
-        visited = [False]*(n+1)
-        self.dfs(1,-1,adj,visited,parent)
-        cycle_nodes = set()
-        node = self.cycle_start
-        while(True):
-            cycle_nodes.add(node)
-            node = parent[node]
-            if node in cycle_nodes:
-                break
-        for i in range(n-1,-1,-1):
-            if edges[i][0] in cycle_nodes and edges[i][1] in cycle_nodes:
-                return edges[i]
+        size = [1]*(n+1)
+        for u,v in edges:
+            if self.union(u,v,parent,size)==False:
+                return [u,v]
