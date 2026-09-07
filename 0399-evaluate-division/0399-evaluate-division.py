d7d29bfae71ea8f22d44b92ef98faa5c0c54bb5b
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-09-07 11:33:51 -0500

    Time: 1 ms (15.06%), Space: 19.6 MB (40.35%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,39 +1,40 @@
-from collections import deque
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = {}
-        q = deque()
+        parent = {}
+        weight = {}
+        def find(x):
+            if x!=parent[x]:
+                old = parent[x]
+                root = find(old)
+                weight[x]*=weight[old]
+                parent[x] = root
+            return parent[x]
+        def union(u,v,value):
+            root_u = find(u)
+            root_v = find(v)
+            if root_u==root_v:
+                return 
+            parent[root_u] = root_v
+            weight[root_u] = value*(weight[v]/weight[u])
         for i in range(len(equations)):
             u = equations[i][0]
             v = equations[i][1]
-            weight = values[i]
-            if u not in adj:
-                adj[u] = []
-            if v not in adj:
-                adj[v] = []
-            adj[u].append([v,weight])
-            adj[v].append([u,1/weight])
+            if u not in parent:
+                parent[u] = u
+                weight[u] = 1
+            if v not in parent:
+                parent[v] = v
+                weight[v] = 1
+            union(u,v,values[i])
         ans = []
         for u,v in queries:
-            if (u not in adj) or (v not in adj):
+            if u not in parent or v not in parent:
                 ans.append(-1)
                 continue
-            if u==v:
-                ans.append(1)
-                continue
-            q = deque()
-            visited = set()
-            q.append((u,1))
-            visited.add(u)
-            result = -1
-            while(q):
-                node,product_so_far = q.popleft()
-                if node==v:
-                    result = product_so_far
-                    break
-                for nei,wt in adj[node]:
-                    if nei not in visited:
-                        q.append((nei,wt*product_so_far))
-                        visited.add(nei)
-            ans.append(result)
+            root_u = find(u)
+            root_v = find(v)
+            if root_u!=root_v:
+                ans.append(-1)
+            else:
+                ans.append(weight[u]/weight[v])
         return ans
