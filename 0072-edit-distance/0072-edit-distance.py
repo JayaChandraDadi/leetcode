5b8116ddb97b3687fa6523a41cf98079c9531093
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-09-19 18:24:19 -0500

    Time: 61 ms (12.71%), Space: 24.3 MB (18.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,6 +1,6 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[-1]*len(word2) for _ in range(len(word1))]
+        dp = [[0]*(len(word2)+1) for _ in range(len(word1)+1)]
         def dfs(i,j):
             if i==len(word1):
                 return len(word2) - j
@@ -16,4 +16,19 @@ class Solution:
                 replace =  1 + dfs(i+1,j+1)
                 dp[i][j] =  min(insert,delete,replace)
             return dp[i][j]
-        return dfs(0,0)
+        n1 = len(word1)
+        n2 = len(word2)
+        for i in range(n1):
+            dp[i][n2] = n1 - i
+        for j in range(n2):
+            dp[n1][j] = n2 - j
+        for i in range(n1-1,-1,-1):
+            for j in range(n2-1,-1,-1):
+                if word1[i]==word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    insert = 1 + dp[i][j+1]
+                    delete = 1 + dp[i+1][j]
+                    replace = 1 + dp[i+1][j+1]
+                    dp[i][j] = min(insert,delete,replace)
+        return dp[0][0]
