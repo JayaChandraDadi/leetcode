ec25dd311b2d82ec2ce9d923c37cc725619f0ed0
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-09-19 22:00:47 -0500

    Time: 1464 ms (6.87%), Space: 22.7 MB (82.03%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,42 +1,36 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergesort(self,head1,head2):
-        dummynode = ListNode()
-        temp = dummynode
-        left = head1
-        right = head2
-        while(left!=None and right!=None):
-            if left.val<=right.val:
-                temp.next  = left
-                left = left.next
+class Solution:
+    def merge_lists(self,head1,head2):
+        dummy = ListNode(-1)
+        temp = dummy
+        temp1 = head1
+        temp2 = head2
+        while temp1 and temp2:
+            if temp1.val<temp2.val:
+                temp.next = temp1
+                temp1 = temp1.next
             else:
-                temp.next = right
-                right = right.next
+                temp.next = temp2
+                temp2 = temp2.next
             temp = temp.next
-        temp.next = left if left else right
-        return dummynode.next
-        
-    def mergeKLists(self, lists):
+        while temp1:
+            temp.next = temp1
+            temp1 = temp1.next
+            temp = temp.next
+        while temp2:
+            temp.next = temp2
+            temp2 = temp2.next
+            temp = temp.next
+        return dummy.next
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return 
         n = len(lists)
-        dummynode = ListNode()
-        ans = dummynode
-        if n==1:
-            return lists[0]
-        i = 1
-        while(len(lists)>1 and i<n):
-            if ans.next==None:
-                ans.next = self.mergesort(lists[i],lists[i-1])
-            else:
-                ans.next = self.mergesort(ans.next,lists[i])
-            i+=1
-        return ans.next
-
-
-        
-
-
-        
+        merged = lists[0]
+        for i in range(1,n):
+            merged = self.merge_lists(merged,lists[i])
+        return merged
